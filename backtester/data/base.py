4ab0cf6f6b71f5bb92757ac56a71fbf6278295e3
iamProud <proud.berlin@gmail.com>
2024-11-05 14:14:16 +0100

    add get candles

--- backtester/data/base.py
+++ backtester/data/base.py
@@ -1,5 +1,9 @@
 # import data.feeds.tradingview as tv
 import data.db as db
+import pandas as pd
+import logging
+
+log = logging.getLogger(__name__)
 
 
 def insert_market(symbol: str, exchange: str, market_type: str, min_move: float):
@@ -17,4 +21,28 @@ def request_data(feed: str, symbol: str, exchange: str):
     fin_db.insert_candles(data, symbol, exchange)
     fin_db.disconnect()
 
-    
+def get(df: pd.DataFrame, symbol: str):
+    sliced_df = df.copy()
+    sliced_df = sliced_df[sliced_df.columns[sliced_df.columns.get_level_values(1) == symbol]]
+    sliced_df.columns = sliced_df.columns.droplevel(1)
+    return sliced_df
+
+def get_candles(feed: str, symbol_ids: list[int], timeframe: int, start_date=None, end_date=None) -> pd.DataFrame:
+    all_dataframes = []
+
+    if feed == "db":
+        fin_db = db.Database()
+
+        for symbol_id in symbol_ids:
+            symbol = fin_db.get_symbol(symbol_id)
+            candles = fin_db.get_candles(symbol_id, timeframe, start_date, end_date)
+            df = pd.DataFrame(data=candles, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
+            df.set_index('timestamp', inplace=True)
+
+            df.columns = pd.MultiIndex.from_product([df.columns, [symbol[0]]])
+
+            all_dataframes.append(df)
+
+        combined_df = pd.concat(all_dataframes, axis=1)
+        
+        return combined_df
--- backtester/data/db.py
+++ backtester/data/db.py
@@ -1,6 +1,10 @@
 import psycopg2
 from psycopg2 import sql
 from decouple import config
+import logging
+import traceback
+
+log = logging.getLogger(__name__)
 
 class Database:
     def __init__(self):
@@ -21,14 +25,15 @@ class Database:
                 user=self.user,
                 password=self.password
             )
-            print("Connected to the database successfully!")
+            log.info("Connected to the database successfully!")
         except (Exception, psycopg2.Error) as error:
-            print("Error while connecting to PostgreSQL:", error)
+            log.error("Error while connecting to PostgreSQL:", error)
+            traceback.print_exc()
 
     def disconnect(self):
         if self.connection:
             self.connection.close()
-            print("Disconnected from the database.")
+            log.info("Disconnected from the database.")
 
     def market_exists(self, symbol, exchange):
         cursor = self.connection.cursor()
@@ -49,7 +54,7 @@ class Database:
         
     def insert_market(self, symbol, exchange, market_type, min_move):
         if self.market_exists(symbol, exchange):
-            print("Market already exists in the database!")
+            log.warning("Market already exists in the database!")
             return
         
         cursor = self.connection.cursor()
@@ -63,7 +68,25 @@ class Database:
             (symbol, exchange, market_type, min_move)
         )
         self.connection.commit()
-        print("Market inserted into PostgreSQL database!")
+        log.info("Market inserted into PostgreSQL database!")
+
+    def get_symbol(self, symbol_id: int):
+        cursor = self.connection.cursor()
+        query = sql.SQL(
+            """
+            SELECT symbol, exchange FROM markets
+            WHERE symbol_id = %s;
+            """
+        )
+
+        cursor.execute(
+            query,
+            (str(symbol_id))
+        )
+
+        symbols = cursor.fetchone()
+
+        return symbols
     
     def get_symbol_id(self, symbol, exchange='%'):
         cursor = self.connection.cursor()
@@ -85,15 +108,14 @@ class Database:
             return None
         
         if len(symbol_id) > 1:
-            print("WARNING: Multiple symbol_ids found! Returning the first one.")
-            print(symbol_id)
+            log.warning("Multiple symbol_ids found! Returning the first one.", symbol_id)
 
         return symbol_id[0][0]
         
     def insert_candles(self, df, symbol, exchange):
         symbol_id = self.get_symbol_id(symbol, exchange)
         if not symbol_id:
-            print(f"Symbol {symbol} not found in the database!")
+            log.warning(f"Symbol {symbol} not found in the database!")
             return
         
         try:
@@ -116,16 +138,84 @@ class Database:
             self.connection.commit()
         
         except psycopg2.DatabaseError as error:
-            print(f"Error: {error}")
+            log.error("Database Error", error)
+            traceback.print_exc()
             if self.connection:
                 self.connection.rollback()
-                print("Transaction rolled back!")
+                log.info("Transaction rolled back!")
 
         except Exception as error:
-            print(f"Error: {error}")
+            log.error("Error", error)
+            traceback.print_exc()
 
         finally:
             if cursor:
                 cursor.close()
-                print("Cursor closed!")
 
+    def get_candles(self, symbol_id, timeframe, start_date=None, end_date=None):
+        try:
+            cursor = self.connection.cursor()
+            
+            # SQL query with conditional date filters
+            query = sql.SQL(
+                """
+                WITH RoundedCandles AS (
+                    SELECT
+                        date_trunc('day', timestamp) + INTERVAL '1 minute' * (
+                            ((EXTRACT(HOUR FROM timestamp)::integer * 60) + EXTRACT(MINUTE FROM timestamp)::integer) - 
+                            ((EXTRACT(HOUR FROM timestamp)::integer * 60 + EXTRACT(MINUTE FROM timestamp)::integer) %% %s)
+                        ) AS rounded_timestamp,
+                        open,
+                        high,
+                        low,
+                        close,
+                        volume,
+                        ROW_NUMBER() OVER (
+                            PARTITION BY symbol_id, date_trunc('day', timestamp) + INTERVAL '1 minute' * (
+                                ((EXTRACT(HOUR FROM timestamp)::integer * 60) + EXTRACT(MINUTE FROM timestamp)::integer) - 
+                                ((EXTRACT(HOUR FROM timestamp)::integer * 60 + EXTRACT(MINUTE FROM timestamp)::integer) %% %s)
+                            )
+                            ORDER BY timestamp ASC
+                        ) AS rn_asc,
+                        ROW_NUMBER() OVER (
+                            PARTITION BY symbol_id, date_trunc('day', timestamp) + INTERVAL '1 minute' * (
+                                ((EXTRACT(HOUR FROM timestamp)::integer * 60) + EXTRACT(MINUTE FROM timestamp)::integer) - 
+                                ((EXTRACT(HOUR FROM timestamp)::integer * 60 + EXTRACT(MINUTE FROM timestamp)::integer) %% %s)
+                            )
+                            ORDER BY timestamp DESC
+                        ) AS rn_desc
+                    FROM candles
+                    WHERE symbol_id = %s
+                    AND (timestamp >= %s OR %s IS NULL)
+                    AND (timestamp <= %s OR %s IS NULL)
+                )
+                SELECT
+                    rounded_timestamp AS timestamp,
+                    MAX(open) FILTER (WHERE rn_asc = 1) AS open,
+                    MAX(high) AS high,
+                    MIN(low) AS low,
+                    MAX(close) FILTER (WHERE rn_desc = 1) AS close,
+                    SUM(volume) AS volume
+                FROM RoundedCandles
+                GROUP BY timestamp
+                ORDER BY timestamp;
+                """
+            )
+
+            # Execute the query with the timeframe, symbol_id, start_date, and end_date parameters
+            cursor.execute(
+                query,
+                (timeframe, timeframe, timeframe, symbol_id, start_date, start_date, end_date, end_date)
+            )
+
+            candles = cursor.fetchall()
+
+            return candles
+
+        except psycopg2.DatabaseError as error:
+            log.error("Database Error", error)
+            traceback.print_exc()
+
+        finally:
+            if cursor:
+                cursor.close()
