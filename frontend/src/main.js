79da3454c9e5d5a72b6b692152e58936b6608d75
Sebastian Stolz <proud.berlin@gmail.com>
2025-05-30 12:33:10 +0200

    feat: add pinia

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,16 +1,15 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import { wsService } from './utils/websocketService'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import router from "./router";
+import { wsService } from "./utils/websocketService";
 
-const app = createApp(App)
+const app = createApp(App);
 
 // Initialize the WebSocket globally
 wsService.connect("ws://localhost:8765");
 app.config.globalProperties.$wss = wsService;
 
-app
-    .use(router)
-    .mount('#app')
+app.use(router).use(createPinia()).mount("#app");
