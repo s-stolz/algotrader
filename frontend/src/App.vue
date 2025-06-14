3b7f89366d8a2e899434f03a49fb333e579c2cbb
Sebastian Stolz <proud.berlin@gmail.com>
2025-06-14 19:38:31 +0200

    refactor: cleanup, fix indentation, renaming

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,83 +1,68 @@
 <template>
-    <div>
-        <!-- <header>
-            <span>
-                <router-link to="/"> Charts </router-link>
-            </span>
-
-            <span>
-                <router-link to="/Data"> Data </router-link>
-            </span>
-        </header> -->
-
-        <main>
-            <router-view />
-        </main>
-    </div>
+  <div>
+    <main>
+      <router-view />
+    </main>
+  </div>
 </template>
 
 <script>
 import Ticket from "./utils/Ticket.js";
-import LightweightChart from "./components/LightweightChart.vue";
 
 export default {
-    components: {
-        "lightweight-chart": LightweightChart,
-    },
+  data() {
+    return {
+      ws: undefined,
+    };
+  },
 
-    data() {
-        return {
-            ws: undefined,
-        };
-    },
+  methods: {},
 
-    methods: {},
+  mounted() {
+    const wss = this.$wss;
 
-    mounted() {
-        const wss = this.$wss;
+    wss.on("message", (data) => {
+      try {
+        let message = JSON.parse(data);
+        // console.log(message);
+      } catch (error) {
+        console.error("Failed to parse message:", error);
+      }
+    });
+  },
 
-        wss.on("message", (data) => {
-            try {
-                let message = JSON.parse(data);
-                // console.log(message);
-            } catch (error) {
-                console.error("Failed to parse message:", error);
-            }
-        });
-    },
-
-    beforeUnmount() {
-        // Clean up the WebSocket connection
-        if (this.$ws) {
-            this.$ws.close();
-        }
-    },
+  beforeUnmount() {
+    // Clean up the WebSocket connection
+    if (this.$ws) {
+      this.$ws.close();
+    }
+  },
 };
 </script>
 
 <style scoped>
 main {
-    padding: 10px;
+  padding: 10px;
 }
 
 header a {
-    display: inline-flex;
-    margin: 10px 8px;
-    padding: 10px 20px;
-    border: none;
-    border-radius: 10px;
-    background: none;
-    color: white;
-    font-size: 20px;
-    font-weight: 600;
-    text-decoration: none;
+  display: inline-flex;
+  margin: 10px 8px;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 10px;
+  background: none;
+  color: white;
+  font-size: 20px;
+  font-weight: 600;
+  text-decoration: none;
 }
 
 header a:hover:not(.router-link-active) {
-    background: rgba(44, 61, 93, 0.5);
+  background: rgba(44, 61, 93, 0.5);
 }
 
 a.router-link-active {
-    background: rgba(44, 61, 93);
+  background: rgba(44, 61, 93);
 }
 </style>
